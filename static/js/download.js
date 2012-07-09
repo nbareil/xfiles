3b06e970b576f2a8219c3d7c5b441fa624abbe96
Nicolas Bareil <nico@chdir.org>
2012-07-09 10:06:25 +0200

    if passphrase not embedded in URL's anchor, provides classic form

--- static/js/download.js
+++ static/js/download.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     var key = window.location.hash.slice(1);
     var keyInput = $('#keyInput')[0];
-    keyInput.setAttribute('type', 'hidden');
-    $('label')[0].setAttribute('style', 'display: none')
-    keyInput.value = key;
-    $('form').submit();
+
+    if (key != "") {
+        keyInput.setAttribute('type', 'hidden');
+        $('label')[0].setAttribute('style', 'display: none')
+        keyInput.value = key;
+        $('form').submit();
+    }
 });
